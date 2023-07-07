14f13278b3d05dbf2800ab3f21bdb5524d755f62
thulane <thulanencholo95@gmail.com>
2023-07-07 20:12:09 +0200

    init

--- server/server/settings.py
+++ server/server/settings.py
@@ -121,9 +121,12 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = ''
 MEDIA_URL = ''
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATIC_ROOT = BASE_DIR / "static"
+
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
